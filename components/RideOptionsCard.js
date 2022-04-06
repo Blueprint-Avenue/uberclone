5c1df5a6d615bbd6517042a30c435f58758887ea
Samiya <skmcintosh20@gmail.com>
2022-04-05 19:35:46 -0700

    implemented price calculator

--- components/RideOptionsCard.js
+++ components/RideOptionsCard.js
@@ -35,6 +35,8 @@ const data = [
 	},
 ];
 
+const SURGE_CHARGE_RATE = 1.5;
+
 const RideOptionsCard = () => {
 	const navigation = useNavigation();
 	const [selected, setSelected] = useState(null);
@@ -75,11 +77,21 @@ const RideOptionsCard = () => {
 							<Text style={tw` text-xl font-semibold`}>{title}</Text>
 							<Text>{travelTimeInformation?.duration.text} Travel Time</Text>
 						</View>
-						<Text style={tw` text-xl`}>$99</Text>
+						<Text style={tw` text-xl`}>
+							{new Intl.NumberFormat("en-us", {
+								style: "currency",
+								currency: "USD",
+							}).format(
+								(travelTimeInformation?.duration.value *
+									SURGE_CHARGE_RATE *
+									multiplier) /
+									100
+							)}
+						</Text>
 					</TouchableOpacity>
 				)}
 			/>
-			<View>
+			<View style={tw`mt-auto border-t border-gray-200`}>
 				<TouchableOpacity
 					disabled={!selected}
 					style={tw`bg-black py-3 m-3 ${!selected && `bg-gray-300`}`}
